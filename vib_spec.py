3bf10946c4a3f3eba0ca7696b4c7201a3b9e0ed1
David Lingerfelt <dblinger@D-172-25-128-143.dhcp4.washington.edu>
2017-07-11 11:36:57 -0700

    added test script

---
+++ vib_spec.py
@@ -0,0 +1,5 @@
+from spectra import Spectra
+import numpy as np
+
+vibrational_spec = Spectra(x='HF_bomd',y='HF_bomd',z='HF_bomd',trans='fourier') # this creates a spectrum object in the variable "vibrational_spec".  This object contains the spectrum (both frequency and intensity axes) 
+
